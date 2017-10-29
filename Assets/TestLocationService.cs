752284d1c8f5697ec2fe9b3f41e57f84faed0df8
daviz <david.muro9@gmail.com>
2017-10-29 17:34:01 +0100

    the whole project in one commit

---
+++ Assets/TestLocationService.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TestLocationService : MonoBehaviour
+{
+    public float latitude;
+    public float longitude;
+
+    public static TestLocationService Instance { get; set;}
+
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(StartLocationService());
+    }
+
+
+    IEnumerator StartLocationService()
+    {
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("User has not enabled GPS");
+            yield break;
+        }
+            
+        // Start service before querying location
+        Input.location.Start();
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)
+        {
+            Debug.Log("Timed out");
+            print("Timed out");
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determine device location");
+            print("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            latitude = Input.location.lastData.latitude;
+            longitude = Input.location.lastData.longitude;
+            // Access granted and location value could be retrieved
+            //print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+        }
+
+        // Stop service if there is no need to query location updates continuously
+        Input.location.Stop();
+    }
+}
---
+++ Assets/UpdateText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateText : MonoBehaviour {
+
+    public Text coordinates;
+
+	void Update () {
+		coordinates.text = "Lat: " + TestLocationService.Instance.latitude.ToString() + "Long: " + TestLocationService.Instance.longitude.ToString();
+	}
+}
